b52bd5edfd40f03f2bd46274845cb997d97ef69b
louloulabeille <louloulabeille@hotmail.com>
2023-09-19 20:04:43 +0200

    Ajout dd'enregistrement de fichier

---
+++ suivi-des-drones/Pages/CreateIncident.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuGet.Common;
+
+namespace suivi_des_drones.Pages
+{
+    public class CreateIncidentModel : PageModel
+    {
+        #region Propriétés
+        [BindProperty]
+        public IFormFile PictureFile { get; set; } = default!;
+
+        private readonly IHostEnvironment _environment;
+        private readonly ILogger<CreateIncidentModel> logger;
+        #endregion
+
+        #region Constructeur
+        public CreateIncidentModel(ILogger<CreateIncidentModel> logger , IHostEnvironment environment)
+        {
+            this.logger = logger;
+            _environment = environment;
+        }
+        #endregion
+
+        #region méthode 
+        public async Task<IActionResult> OnPost()
+        {
+            IActionResult result = Page();
+
+            using var file = new FileStream(_environment.ContentRootPath+"./" + PictureFile.FileName, FileMode.OpenOrCreate);
+            await PictureFile.CopyToAsync(file);
+
+            return result;
+        }
+
+        public void OnGet()
+        {
+        }
+        #endregion
+    }
+}
---
+++ suivi-des-drones/Pages/Incident.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace suivi_des_drones.Pages
+{
+    public class IncidentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
